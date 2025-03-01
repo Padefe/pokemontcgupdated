6c953a8607e87352bb3060dcce4e9a503024dce3
Padefe <thegamechair@gmail.com>
2025-03-01 21:20:26 +0100

    Fixed a route problem

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import pokemonRoutes from '../routes/cardRoutes.js';
+import cardRoutes from '../routes/cardRoutes.js';
 import { config } from 'dotenv';
 import { fetchPokemonCards } from '../utils/fetchPokemonCards.js';
 
