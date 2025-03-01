8d24efe55271bb60aa82112850f319b60184131a
Padefe <thegamechair@gmail.com>
2025-03-01 22:38:08 +0100

    Updated

--- config/supabase.js
+++ config/supabase.js
@@ -1,6 +1,13 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
+
+console.log('Supabase URL:', supabaseUrl);
+console.log('Supabase Key:', supabaseKey);
+
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Missing Supabase URL or Key');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
