ccbe8cd1f66841fe8b6d7ac7e3eb8774b28844ea
Padefe <thegamechair@gmail.com>
2025-03-01 20:55:59 +0100

    Added html

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 import express from 'express';
 import pokemonRoutes from './routes/cardRoutes.js';
 import { config } from 'dotenv';
+import { fetchPokemonCards } from './utils/fetchPokemonCards.js';
 
 config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 app.use('/api', pokemonRoutes);
+
+app.get('/cards', async (req, res) =>
+{
+    try
+    {
+        const cards = await fetchPokemonCards();
+        res.render('cards', { cards });
+    }
+    catch (error)
+    {
+        res.status(500).json({ error: 'failed to fetch cards' });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
