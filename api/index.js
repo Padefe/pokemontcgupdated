124b5a81a839568c6a6ecdedd90fcebe50839298
Padefe <thegamechair@gmail.com>
2025-03-03 17:55:34 +0100

    Updated

--- api/index.js
+++ api/index.js
@@ -7,11 +7,13 @@ import { config } from 'dotenv';
 
 config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api', cardRoutes);
 
