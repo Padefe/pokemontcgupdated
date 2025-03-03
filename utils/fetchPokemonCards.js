89c197bbfef59c936c4c7376735ef36f5e97c705
Padefe <thegamechair@gmail.com>
2025-03-03 21:58:53 +0100

    updated

--- utils/fetchPokemonCards.js
+++ utils/fetchPokemonCards.js
@@ -2,7 +2,7 @@ import {supabase} from '../config/supabase.js';
 
 export async function fetchPokemonCards() 
 {
-    const { data, error } = await supabase.from('User_cards').select('*');
+    const { data, error } = await supabase.from('User_cards').select('*').join('Cards', 'Cards.id', 'User_cards.card_id');
 
     if (error) {
         console.error('fetch.js: Error fetching cards:', error);
