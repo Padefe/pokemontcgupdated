b743cf79754b4c08c70fb760f8b95d816c4a2000
Padefe <thegamechair@gmail.com>
2025-03-03 18:33:24 +0100

    Added collection

--- api/index.js
+++ api/index.js
@@ -13,33 +13,27 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api', cardRoutes);
 
 app.get('/', (req, res) => {
-    console.log("test index.js 2");
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 
-
 app.get('/collection', async (req, res) =>
 {
-    console.log("test index.js 3");
     try
     {
-        console.log('Route /collection hit');
         const cards = await fetchPokemonCards();
-        res.render('collection', { cards });
+        res.sendFile(path.join(__dirname, '../public/collection.html'));
     }
     catch (error)
     {
         res.status(500).json({ error: 'index.js: failed to fetch cards' });
     }
 });
-
 
 export default serverless(app);
 module.exports = app;
