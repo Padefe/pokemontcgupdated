23223767e608b6b1c956503bf0c3ec1bb4793e2a
Padefe <thegamechair@gmail.com>
2025-03-03 17:43:29 +0100

    Changed from .ejs to static

--- api/index.js
+++ api/index.js
@@ -1,33 +1,22 @@
 import express from 'express';
 import serverless from 'serverless-http';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import cardRoutes from '../routes/cardRoutes.js';
 import { config } from 'dotenv';
 //import { fetchPokemonCards } from '../utils/fetchPokemonCards.js';
 
 config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
 
-app.set('view engine', 'ejs');
-
-
-app.set('views', path.join(__dirname, '../views'));
-
-app.use(express.static(path.join(__dirname, '../public')));
-
+app.use(express.static('public'));
 
 app.use('/api', cardRoutes);
 
-
 app.get('/', (req, res) => {
     console.log("test index.js 2");
-    res.render('index');
+    res.sendFile('index.html');
 });
 
 
