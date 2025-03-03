33c96554d6d195c43f146a5b13fa509b6b1bc2f9
Padefe <thegamechair@gmail.com>
2025-03-03 17:37:23 +0100

    Updated

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import serverless from 'serverless-http';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import cardRoutes from '../routes/cardRoutes.js';
 import { config } from 'dotenv';
 //import { fetchPokemonCards } from '../utils/fetchPokemonCards.js';
 
 config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -13,10 +17,9 @@ const app = express();
 app.set('view engine', 'ejs');
 
 
-app.set('views', './views');
+app.set('views', path.join(__dirname, '../views'));
 
-
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.use('/api', cardRoutes);
