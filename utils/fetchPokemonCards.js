50711fdd96f82ee1b282e39ec5222fb121cf606f
Padefe <thegamechair@gmail.com>
2025-03-03 22:08:14 +0100

    updated

--- utils/fetchPokemonCards.js
+++ utils/fetchPokemonCards.js
@@ -5,7 +5,6 @@ export async function fetchPokemonCards()
     const { data, error } = await supabase
     .from('User_cards')
     .select('*')
-    .join('Card', 'card.id', 'User_cards.card_id')
     .eq('User_cards.user_id', "9ec32a6f-a6f5-4b1f-a342-88a6d607ac05");
 
     if (error) {
